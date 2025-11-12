efd4f5f66e64d37a788faaff0b079eac34cbf1c3
Moreira <hugo.moreira@ingrammicro.com>
2025-11-12 15:08:30 -0300

    setup inicial do spring

---
+++ backend/site-de-pesca/site-de-pesca/src/main/java/site_de_pesca/site_de_pesca/SiteDePescaApplication.java
@@ -0,0 +1,13 @@
+package site_de_pesca.site_de_pesca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteDePescaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteDePescaApplication.class, args);
+	}
+
+}
---
+++ backend/site-de-pesca/site-de-pesca/src/test/java/site_de_pesca/site_de_pesca/SiteDePescaApplicationTests.java
@@ -0,0 +1,13 @@
+package site_de_pesca.site_de_pesca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteDePescaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
